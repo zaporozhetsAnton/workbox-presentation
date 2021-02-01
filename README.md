158f8e039ad8c49bdbf57781b87f317c9b14a8cb
Anton Zaporozhets <zaporozhetsanton@gmail.com>
2021-02-01 09:52:02 +0200

    added readme

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # workbox-presentation
+
+Presentation of basic workbox functionality.
+
+Two examples:
+ - workboxPlugin.GenerateSW - generates service worker that does precaching of dist folder and includes runtime caching logic for image requests. Example written in webpack.prod.config.js file.
+ - workboxPlugin.InjectManifest - generate service worker by template, also does precaching of dist folder and includes runtime caching logic for image requests. Example written in webpack.prod.config.j and src/service-worker.js files.
+
+You should use one of these examples, workboxPlugin.GenerateSW for something simple and workboxPlugin.InjectManifest for complex service workers.
+
+webpack/typescript/eslint configuration copied from [zaporozhetsAnton/react-webpack-typescript](https://github.com/zaporozhetsAnton/react-webpack-typescript)
--- src/service-worker.js
+++ src/service-worker.js
@@ -10,36 +10,36 @@ import { precacheAndRoute } from 'workbox-precaching';
 precacheAndRoute(self.__WB_MANIFEST);
 
 // example of image requests caching
-import { registerRoute } from 'workbox-routing';
-import { CacheFirst } from 'workbox-strategies';
-import { CacheableResponsePlugin } from 'workbox-cacheable-response';
-import { ExpirationPlugin } from 'workbox-expiration';
-
-const cacheName = 'images';
-const matchCallback = ({ request }) => request.destination === 'image';
-const maxAgeSeconds = 30 * 24 * 60 * 60; // 30 days
-const maxEntries = 60;
-
-registerRoute(
-  matchCallback,
-  new CacheFirst({
-    cacheName,
-    plugins: [
-      new CacheableResponsePlugin({
-        statuses: [200],
-      }),
-      new ExpirationPlugin({
-        maxEntries,
-        maxAgeSeconds,
-      }),
-    ],
-  }),
-);
+// import { registerRoute } from 'workbox-routing';
+// import { CacheFirst } from 'workbox-strategies';
+// import { CacheableResponsePlugin } from 'workbox-cacheable-response';
+// import { ExpirationPlugin } from 'workbox-expiration';
+//
+// const cacheName = 'images';
+// const matchCallback = ({ request }) => request.destination === 'image';
+// const maxAgeSeconds = 30 * 24 * 60 * 60; // 30 days
+// const maxEntries = 60;
+//
+// registerRoute(
+//   matchCallback,
+//   new CacheFirst({
+//     cacheName,
+//     plugins: [
+//       new CacheableResponsePlugin({
+//         statuses: [200],
+//       }),
+//       new ExpirationPlugin({
+//         maxEntries,
+//         maxAgeSeconds,
+//       }),
+//     ],
+//   }),
+// );
 
 // example of workbox recipe, it does the same as upper image requests caching example
-import { imageCache } from 'workbox-recipes';
-
-imageCache();
+// import { imageCache } from 'workbox-recipes';
+//
+// imageCache();
 
 // workbox strategies https://developers.google.com/web/tools/workbox/modules/workbox-strategies
 // workbox recipes https://developers.google.com/web/tools/workbox/modules/workbox-recipes
